4b225949bfee041b518339b1e50139d4dd043e9c
TibixDev <tibix@fhs.sh>
2026-08-18 08:25:24 +0300

    chore: Bump version (app, helios, image)

--- build-guest-server.sh
+++ build-guest-server.sh
@@ -10,9 +10,9 @@ export VERSION="$(bun -p "require('./package.json').version")"
 export COMMIT_HASH="$(git rev-parse --short HEAD)"
 export BUILD_TIMESTAMP=$(date '+%Y-%m-%dT%H:%M:%S')
 
-# Helios wddm bundle at f2358882
+# Helios WDDM bundle at 7e0e6b73
 HELIOS_REPOSITORY="winboat-org/helios"
-HELIOS_RUN_ID="31297503984"
+HELIOS_RUN_ID="31701467522"
 HELIOS_ARTIFACT="helios-windows-x64-22.22.259.0"
 HELIOS_DOWNLOAD_DIR=""
 
