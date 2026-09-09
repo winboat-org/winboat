8e88b24649bed2b8e46564d369a103dad28d1606
TibixDev <tibix@fhs.sh>
2026-09-09 15:58:34 +0300

    Release 1.0.7 with updated Helios and 60 FPS RDP defaults

--- build-guest-server.sh
+++ build-guest-server.sh
@@ -10,10 +10,10 @@ export VERSION="$(bun -p "require('./package.json').version")"
 export COMMIT_HASH="$(git rev-parse --short HEAD)"
 export BUILD_TIMESTAMP=$(date '+%Y-%m-%dT%H:%M:%S')
 
-# Helios WDDM bundle at 7e0e6b73
+# Helios WDDM bundle at fcfd99e0
 HELIOS_REPOSITORY="winboat-org/helios"
-HELIOS_RUN_ID="31701467522"
-HELIOS_ARTIFACT="helios-windows-x64-22.22.259.0"
+HELIOS_RUN_ID="34324087456"
+HELIOS_ARTIFACT="helios-windows-x64-22.22.270.0"
 HELIOS_DOWNLOAD_DIR=""
 
 cleanup() {
